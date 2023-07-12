5076f2271915877511b188f1cdc51c9c666057db
LuigiEspinosa <numerocuatro@pm.me>
2023-07-12 01:58:25 -0500

    feat: Coming Soon Page

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import Metadata from 'next';
+import '../app.scss';
+
+export const metadata: Metadata = {
+  title: 'Luigi Espinosa | Frontend web developer',
+  description: 'Número Cuatro Portfolio',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  );
+}
+
