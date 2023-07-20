df6af2f5feee4a51d29c6c0bf5f3e14e4c83379d
LuigiEspinosa <numerocuatro@pm.me>
2023-07-19 21:06:47 -0500

    fix: Remove next/image

--- src/atoms/Logo/Logo.tsx
+++ src/atoms/Logo/Logo.tsx
@@ -1,14 +1,8 @@
-import Image from 'next/image';
 import './logo.scss';
 
 export const Logo = () => (
   <div className='logo'>
-    <Image
-      src={`${process.env.NEXT_PUBLIC_FIREBASE_DOMAIN}/images/logo.png`}
-      width={368}
-      height={132}
-      alt='Numero Cuatro Logo in white'
-      priority={true}
-    />
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img src='/images/logo.png' width={368} height={132} alt='Numero Cuatro Logo in white' />
   </div>
 );
