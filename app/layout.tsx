fa1d655395a7950bbb69b9880ee840490ade8951
Cuatro <luigi@cuatro.dev>
2026-03-08 15:24:04 -0500

    feat(scroll): Add Lenis and ScrollTrigger provider (#23)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
+import { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import './app.scss';
 
 import { Header } from '@/components/molecules/Header/Header';
 import { Body } from '@/components/atoms/Container/Container';
+import { Providers } from '@/app/providers';
 
 export const metadata: Metadata = {
-  title: 'Luigi Espinosa | Frontend web developer',
-  description: "Número Cuatro's Portfolio",
+  title: 'Luigi Espinosa | Senior Frontend Engineer',
+  description: "Cuatro's Portfolio",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
       <Body>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </Body>
     </html>
   );
