e7a13216d4b6e8d3de1c0862d5e05311f8593fdc
Cuatro <luigi@cuatro.dev>
2026-04-04 02:30:54 -0500

    fix(gem): Add static fallback when WebGL is unavailable (#67)

--- components/molecules/GemComponent/GemComponent.tsx
+++ components/molecules/GemComponent/GemComponent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import { ParticleWave } from '@/components/atoms/ParticleWave/ParticleWave';
@@ -8,15 +9,37 @@ const Scene = dynamic(() => import('@/components/atoms/Scene/Scene').then((m) =>
   ssr: false,
 });
 
-const GemComponent = () => (
-  <div id='gem-canvas'>
-    <Scene>
-      <ParticleWave />
-      <EffectComposer>
-        <Bloom luminanceThreshold={0.2} intensity={0.55} radius={0.4} mipmapBlur />
-      </EffectComposer>
-    </Scene>
-  </div>
-);
+const GemComponent = () => {
+  // null = not yet checked (avoids fallback flash on capable devices)
+  const [webglAvailable, setWebglAvailable] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const canvas = document.createElement('canvas');
+    const ctx =
+      canvas.getContext('webgl') ??
+      (canvas.getContext('experimental-webgl') as WebGLRenderingContext | null);
+    setWebglAvailable(!!ctx);
+  }, []);
+
+  if (webglAvailable === false) {
+    return (
+      <div id='gem-canvas'>
+        {/* Static screenshort shown on devices without WebGL support */}
+        <img src='/assets/home/gem-fallback.png' alt='' aria-hidden='true' />
+      </div>
+    );
+  }
+
+  return (
+    <div id='gem-canvas'>
+      <Scene>
+        <ParticleWave />
+        <EffectComposer>
+          <Bloom luminanceThreshold={0.2} intensity={0.55} radius={0.4} mipmapBlur />
+        </EffectComposer>
+      </Scene>
+    </div>
+  );
+};
 
 export default GemComponent;
