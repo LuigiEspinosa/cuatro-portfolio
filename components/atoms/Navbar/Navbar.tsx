13092e8f828089af6999dfc71874e13348d58711
Cuatro <luigi@cuatro.dev>
2026-04-04 02:13:16 -0500

    chore(navbar): Add rel to external links (#64)

--- components/atoms/Navbar/Navbar.tsx
+++ components/atoms/Navbar/Navbar.tsx
@@ -6,14 +6,16 @@ export const Navbar = () => (
     <Link href='/work'>Work</Link>
     <Link href='/projects'>Projects</Link>
     <Link href='/blog'>Blog</Link>
-    <Link href='https://www.github.com/luigiespinosa' target='_blank'>
+    <Link href='https://www.github.com/luigiespinosa' target='_blank' rel='noopener noreferrer'>
       Github
     </Link>
-    <Link href='https://www.linkedin.com/in/luigiespinosa' target='_blank'>
+    <Link
+      href='https://www.linkedin.com/in/luigiespinosa'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
       LinkedIn
     </Link>
-    <Link href='mailto:luigi@cuatro.dev' target='_blank'>
-      Contact Me
-    </Link>
+    <Link href='mailto:luigi@cuatro.dev'>Contact Me</Link>
   </nav>
 );
